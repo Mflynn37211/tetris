11c86f6446e27f7c922588c407eeafdcaacfd0cc
Michael Flynn <mflynn37211@yahoo.com>
2014-09-27 13:40:36 -0400

    added function so graphics are seen in browser

--- js/src/Game.js
+++ js/src/Game.js
@@ -2,8 +2,21 @@ define(function(){
 
   var Game = Class.extend({
 
+    tick: function() {
+      console.warn("should be overwritten by childclass!")
+    },
+
     run: function() {
-      console.log("running");
+
+        var self = this;
+        function loop() {
+          self._reqframe = window.requestAnimationFrame(loop);
+
+          self.tick();
+
+          canvas.flip();
+        }
+        this._reqframe = window.requestAnimationFrame(loop);
     }
   });
 
--- js/src/main.js
+++ js/src/main.js
@@ -14,14 +14,20 @@ require(["Game"], function(Game) {
     var App = Game.extend({
 
       init: function() {
-        canvas.width = 200;
-        canvas.height = 100;
-        canvas.scale = 2;
+        canvas.width = 480;
+        canvas.height = 272;
+        canvas.scale = 1;
 
         content.load("back", "res/back.png")
         content.load("blocks", "res/blocks.png")
         content.load("numbers", "res/numbers.png")
-      }
+      },
+
+        tick: function(){
+          if (content.progress() === 1) {
+            canvas.ctx.drawImage(content.get("back"), 0, 0);
+          }
+        }
     });
 
    (function() {
