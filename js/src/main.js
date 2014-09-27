7b27da8eecee069dede71b26eb5e189e8ec7ee43
Michael Flynn <mflynn37211@yahoo.com>
2014-09-27 12:24:05 -0400

    Initial commit Added all the require files for simple javascript inheritance added js src files added js class files added js engine file added canvas added index html added styles css

---
+++ js/src/main.js
@@ -0,0 +1,27 @@
+
+
+requirejs.config({
+
+  baseURL: "js",
+
+  paths: {
+      src:"./src"
+  }
+});
+
+require(["src/Game"]), function(Game) {
+
+    var App = Game.extend({
+
+      init: function() {
+        canvas.width = 200;
+        canvas.height = 100;
+        canvas.scale = 2;
+      }
+    });
+
+    window.onload = function() {
+        var game = new App();
+        game.run();
+    }
+});
